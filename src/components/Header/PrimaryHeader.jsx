242006bcf087dd8a563d7ec2446066df27bc8f3a
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2023-05-11 20:06:06 -0600

    fixes in navbar according to new design

--- src/components/Header/PrimaryHeader.jsx
+++ src/components/Header/PrimaryHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PrimaryButton from '../Buttons/PrimaryButton/PrimaryButton';
 import Logo from '../Logos/Primary Logo/Logo';
 import OptionMenu from '../Menus/OptionMenu';
 import OffCanvas from '../Sidebars/PrimaryOffCanvas';
@@ -14,22 +15,20 @@ const PrimaryHeader = () => {
             nav: '/services'
         },
         {
-            name: '¡Conóceme!',
-            nav: '/about'
-        },
-        {
-            name: 'Galería',
+            name: 'Galería!',
             nav: '/about'
         }
 
     ];
 
     return (
-        <div className="container__header--sticky d-flex align-items-center justify-content-around">
+        <div className="container__header--sticky d-flex align-items-center justify-content-between">
             <OffCanvas anchorClasses="text-center w-100 off-canvas__anchor-text--32px" />
             <Logo/>
-            <div className="option__menu d-none d-md-flex">
+            <div className="option__menu me-4 d-none d-md-flex">
                 <OptionMenu displayOptions={optionMenu} customClasses="d-flex align-items-center container__header-options" anchorClasses="text-center px-3" />
+                <PrimaryButton variant="primary" text="Contacto" customCss="rounded-pill header__contact-button"/>
+
             </div>
         </div>
     );
--- src/components/Logos/Primary Logo/Logo.jsx
+++ src/components/Logos/Primary Logo/Logo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from '../../../assets/logo/sky-dog-logo.png';
+// import logo from '../../../assets/logo/sky-dog-logo.png';
+import logo from '../../../assets/logo/main_logo.svg';
 import { useNavigate } from 'react-router-dom';
 
 const Logo = () => {
     const navigate = useNavigate();
     return (
-        <>
+        <div className='logo__container ms-4'>
             <img className="logo__sky-dog" onClick={() => navigate("/")} src={logo} alt="logo" />
-        </>
+        </div>
     );
 };
 
